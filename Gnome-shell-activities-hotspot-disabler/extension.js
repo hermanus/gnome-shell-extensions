9ba288d5567bfd1b01a0cd2b08528a01927a0870
Nathaniel Case <Qalthos@gmail.com>
2011-12-12 23:22:12 -0500

    First attempt at massaging the 3.0 flow into 3.2.

--- Gnome-shell-activities-hotspot-disabler/extension.js
+++ Gnome-shell-activities-hotspot-disabler/extension.js
@@ -7,18 +7,18 @@
 
 const Panel = imports.ui.layout;
 
-function main() {
-//clear the _onCornerEntered function so there will be no hotspot    
-    Panel.HotCorner.prototype._onCornerEntered = function() {
-			return false;
-		};
-}
-
 function init() {
+    old_function = Panel.HotCorner.prototype._onCornerEntered;
 }
 
 function enable() {
+    //clear the _onCornerEntered function so there will be no hotspot
+    Panel.HotCorner.prototype._onCornerEntered = function() {
+        return false;
+    };
 }
 
 function disable() {
+    //restore the _onCornerEntered function
+    Panel.HotCorner.prototype._onCornerEntered = old_function;
 }
