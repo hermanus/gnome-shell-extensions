abc0e2ebaf783ba7eb8eda66ca5be42185a69576
Nathaniel Case <Qalthos@gmail.com>
2011-12-12 22:54:20 -0500

    First step to GNOME 3.2-ification.

--- Gnome-shell-activities-hotspot-disabler/extension.js
+++ Gnome-shell-activities-hotspot-disabler/extension.js
@@ -5,11 +5,20 @@
  * 
  */
 
-const Panel = imports.ui.panel;
+const Panel = imports.ui.layout;
 
 function main() {
 //clear the _onCornerEntered function so there will be no hotspot    
     Panel.HotCorner.prototype._onCornerEntered = function() {
 			return false;
 		};
+}
+
+function init() {
+}
+
+function enable() {
+}
+
+function disable() {
 }
