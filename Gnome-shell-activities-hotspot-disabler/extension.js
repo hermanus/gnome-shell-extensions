eb108794965205e71371a47bd32494bb792de602
Nathaniel Case <Qalthos@gmail.com>
2012-04-11 16:46:48 -0400

    Added a function to kill other hotcorners and do so on every monitor change event.  Removed prototype function because it's pretty much useless.

--- Gnome-shell-activities-hotspot-disabler/extension.js
+++ Gnome-shell-activities-hotspot-disabler/extension.js
@@ -8,16 +8,30 @@
 const Main = imports.ui.main;
 const Panel = imports.ui.layout;
 
+let _monitorChangeId;
+
 function init() {
-    Panel.HotCorner.prototype.setReactive = function(enable) {
-        this._corner.reactive = enable;
-    }
 }
 
 function enable() {
-    Main.panel._activitiesButton._hotCorner.setReactive(false);
+    hotCorners(false);
+    Main.panel._activitiesButton._hotCorner._corner.reactive = false;
+    _monitorChangeId = Main.layoutManager.connect('monitors-changed', disableHotCorners);
 }
 
 function disable() {
-    Main.panel._activitiesButton._hotCorner.setReactive(true);
+    hotCorners(true);
+    Main.panel._activitiesButton._hotCorner._corner.reactive = true;
+    Main.layoutManager.disconnect(_monitorChangeId);
+}
+
+function disableHotCorners() {
+    hotCorners(false);
+}
+
+function hotCorners(enable) {
+    for(let i = 0; i < Main.layoutManager._hotCorners.length; i++) {
+        hotcorner = Main.layoutManager._hotCorners[i];
+        hotcorner._corner.reactive = enable;
+    }
 }
