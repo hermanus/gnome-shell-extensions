be85b4846526cac509a761db6261ba945aa40c33
Nathaniel Case <Qalthos@gmail.com>
2013-12-11 10:51:15 -0500

    Looks unnecessary

--- Gnome-shell-activities-hotspot-disabler/extension.js
+++ Gnome-shell-activities-hotspot-disabler/extension.js
@@ -13,13 +13,11 @@ function init() {}
 
 function enable() {
     hotCorners(false);
-    //Main.panel.statusArea.activities.hotCorner._corner.reactive = false;
     _monitorChangeId = Main.layoutManager.connect('monitors-changed', disableHotCorners);
 }
 
 function disable() {
     hotCorners(true);
-    //Main.panel.statusArea.activities.hotCorner._corner.reactive = true;
     Main.layoutManager.disconnect(_monitorChangeId);
 }
 
